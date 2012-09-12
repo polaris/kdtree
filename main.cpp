cba87375ebbd31b16152181cb0afadbabb91a24b
Jan <jan.deinhard@gmail.com>
2012-09-12 22:41:48 +0200

    add hyper rect

--- main.cpp
+++ main.cpp
@@ -18,14 +18,20 @@ struct Point3d
         v_[2] = z;
     }
 
+    float operator [](int i) const
+    {
+        // TODO: add bounds check
+        return v_[i];
+    }
+
     float v_[3];
 };
 
 static float distance(const Point3d &p, const Point3d &q)
 {
-    const float dx = q.v_[0] - p.v_[0];
-    const float dy = q.v_[1] - p.v_[1];
-    const float dz = q.v_[2] - p.v_[2];
+    const float dx = q[0] - p[0];
+    const float dy = q[1] - p[1];
+    const float dz = q[2] - p[2];
     return sqrtf(dx * dx + dy * dy + dz * dz);
 }
 
@@ -39,7 +45,7 @@ public:
 
     bool operator ()(const Point3d &a, const Point3d &b)
     {
-        return a.v_[axis_] < b.v_[axis_];
+        return a[axis_] < b[axis_];
     }
 
 private:
@@ -47,10 +53,60 @@ private:
 };
 
 template <int DIM>
+struct KdHyperRect
+{
+    KdHyperRect()
+    {
+        memset(min, 0, DIM * sizeof(float));
+        memset(max, 0, DIM * sizeof(float));
+    }
+
+    KdHyperRect(const std::vector<Point3d> &pts)
+    {
+        memset(min, 0, DIM * sizeof(float));
+        memset(max, 0, DIM * sizeof(float));
+
+        for (size_t i = 0; i < pts.size(); i++) {
+            extend(pts[i]);
+        }
+    }
+
+    void extend(const Point3d &pt)
+    {
+        for (int i = 0; i < DIM; ++i) {
+            if (pt[i] < min[i]) {
+                min[i] = pt[i];
+            }
+            if (pt[i] > max[i]) {
+                max[i] = pt[i];
+            }
+        }
+    }
+
+    float dist_sq(const Point3d &pt) const
+    {
+        float result = 0;
+        for (int i = 0; i < DIM; i++) {
+            if (pt[i] < min[i]) {
+                const float d = min[i] - pt[i];
+                result += d * d;
+            } else if (pt[i] > max[i]) {
+                const float d = max[i] - pt[i];
+                result += d * d;
+            }
+        }
+        return result;
+    }
+
+    float min[DIM];
+    float max[DIM];
+};
+
+template <int DIM>
 struct KdTreeNode
 {
     KdTreeNode(int axis, const Point3d &pt)
-    : axis_(axis), pt_(pt), position_(pt.v_[axis]), left_(0), right_(0)
+    : axis_(axis), pt_(pt), position_(pt[axis]), left_(0), right_(0)
     {
     }
 
@@ -67,7 +123,7 @@ struct KdTreeNode
         std::vector<Point3d> left(temp.begin(), temp.begin() + median);
         std::vector<Point3d> right(temp.begin() + median, temp.end());
 
-        position_ = temp[median].v_[axis_];
+        position_ = temp[median][axis_];
 
         if (left.size() > 1) {
             left_ = new KdTreeNode(axis_ + 1 % DIM, left);
@@ -102,18 +158,18 @@ struct KdTreeNode
                 res = pt_;
             }
         } else {
-            if (pt.v_[axis_] < position_) {
-                if (pt.v_[axis_] - d < position_) {
+            if (pt[axis_] < position_) {
+                if (pt[axis_] - d < position_) {
                     left_->nearest(pt, res, d);
                 }
-                if (pt.v_[axis_] + d >= position_) {
+                if (pt[axis_] + d >= position_) {
                     right_->nearest(pt, res, d);
                 }
             } else {
-                if (pt.v_[axis_] + d >= position_) {
+                if (pt[axis_] + d >= position_) {
                     right_->nearest(pt, res, d);
                 }
-                if (pt.v_[axis_] - d < position_) {
+                if (pt[axis_] - d < position_) {
                     left_->nearest(pt, res, d);
                 }
             }
@@ -131,6 +187,7 @@ class KdTree
 {
 public:
     KdTree(const std::vector<Point3d> &pts)
+    : rect_(pts)
     {
         root_ = new KdTreeNode<DIM>(0, pts);
     }
@@ -150,22 +207,19 @@ public:
 
 private:
     KdTreeNode<DIM> *root_;
+    KdHyperRect<DIM> rect_;
 };
 
 int main()
 {
     std::vector<Point3d> pts;
     pts.push_back(Point3d(0, 0, 0));
-    pts.push_back(Point3d(0, 0, 1));
-    pts.push_back(Point3d(0, 1, 0));
-    pts.push_back(Point3d(0, 1, 1));
-    pts.push_back(Point3d(1, 0, 0));
-    pts.push_back(Point3d(1, 0, 1));
-    pts.push_back(Point3d(1, 1, 0));
-    pts.push_back(Point3d(1, 1, 1));
+    pts.push_back(Point3d(0, 2, 0));
+    pts.push_back(Point3d(2, 0, 0));
+    pts.push_back(Point3d(2, 2, 0));
 
     KdTree<3> t2(pts);
 
-    Point3d q(0.39f, 0.39f, 0.39f), res(-1, -1, -1);
+    Point3d q(2, -1, 0), res(-1, -1, -1);
     t2.nearest(q, res);
 }
